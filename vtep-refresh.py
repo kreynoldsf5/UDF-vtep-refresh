3ba198fde2030c3c33acc6cde8e6b019e7c159f7
wwr2d2d <me@wwr2d2d.org>
2019-04-23 09:15:45 -0400

    updates

--- vtep-refresh.py
+++ vtep-refresh.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import os
+import sys
 
 #import needed vars from env
 bipMGMT = os.getenv('BIP')
@@ -14,13 +15,34 @@ k8 = os.getnenv('K8')
 k8Token = os.getenv('K8TOKEN')
 
 #Find new VTEP MAC address on the BIG-IP
-br = requests.get('https://{}/mgmt/tm/net/tunnels/tunnel/~Common~flannel_vxlan/stats?options=all-properties'.format(bipMGMT), verify=False, auth=HTTPBasicAuth(bipUser, bipUser))
-vtepMAC = br.json().entries.https://localhost/mgmt/tm/net/tunnels/tunnel/~Common~flannel_vxlan/~Common~flannel_vxlan/stats.nestedStats.entries.macAddr.description
+try:
+    br = requests.get('https://{}/mgmt/tm/net/tunnels/tunnel/~Common~flannel_vxlan/stats?options=all-properties'.format(bipMGMT), verify=False, auth=HTTPBasicAuth(bipUser, bipUser))
+    br.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
+    sys.exit(1)
+except requests.exceptions.RequestException as err:
+    print(err)
+    sys.exit(1)
+try:
+    vtepMAC = br.json().entries.https://localhost/mgmt/tm/net/tunnels/tunnel/~Common~flannel_vxlan/~Common~flannel_vxlan/stats.nestedStats.entries.macAddr.description
+except json.decoder.JSONDecodeError as err:
+    print(err)
+    sys.exit(1)
 
-#Setup our node payload
+#Create our node payload
 node = {"kind": "Node", "spec": {"podCIDR": bipPodCIDR}, "apiVersion": "v1", "metadata": {"name": bipName, "annotations": {"flannel.alpha.coreos.com/backend-data": {"VtepMAC": vtepMAC}, "flannel.alpha.coreos.com/public-ip": bipFlanPIP, "flannel.alpha.coreos.com/backend-type": "vxlan", "flannel.alpha.coreos.com/kube-subnet-manager": "true"}}}
 
 #Make request to kube master
 headers = {'Content-type': 'application/json', 'Authorization': 'Bearer {}'.format(k8Token)}
-kr = requests.post('https://{}/api'.format(k8), verify=False, headers=headers, data=json.dumps(node))
-print(kr.status_code)
+try:
+    kr = requests.post('https://{}/api'.format(k8), verify=False, headers=headers, data=json.dumps(node))
+    kr.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
+    sys.exit(1)
+except requests.exceptions.RequestException as err:
+    print(err)
+    sys.exit(1)
+print(kr.json())
+sys.exit(0)
